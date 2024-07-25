3a2858d25db6ec8a94350d87be121600fbe64b1f
Raso Vukicevic <rasovukicevic98@gmail.com>
2024-07-25 13:10:31 +0200

    Implemented Adapter patter.

---
+++ CustomerService/Contracts/IUserServiceAdapter.cs
@@ -0,0 +1,7 @@
+﻿namespace CustomerService.Contracts
+{
+    public interface IUserServiceAdapter
+    {
+        Task<(bool exists, string userName)> UserExistsAsync(int userId);
+    }
+}
--- CustomerService/Services/DiscountService.cs
+++ CustomerService/Services/DiscountService.cs
@@ -2,6 +2,7 @@
 using CSharpFunctionalExtensions;
 using CSharpFunctionalExtensions.ValueTasks;
 using CsvHelper;
+using CustomerService.Contracts;
 using CustomerService.Contracts.Repositories;
 using CustomerService.Contracts.Services;
 using CustomerService.Dtos;
@@ -20,77 +21,16 @@ namespace CustomerService.Services
     {
         private readonly IDiscountRepository _repository;
         private readonly IMapper _mapper;
-        private readonly HttpClient _httpClient;
         private readonly IAgentRepository _agentRepository;
-
-        public DiscountService(IDiscountRepository repository, IMapper mapper, HttpClient httpClient, IAgentRepository agentRepository)
+        private readonly IUserServiceAdapter _userServiceAdapter;
+        public DiscountService(IDiscountRepository repository, IMapper mapper, IAgentRepository agentRepository, IUserServiceAdapter userServiceAdapter)
         {
             _repository = repository;
             _mapper = mapper;
-            _httpClient = httpClient;
             _agentRepository = agentRepository;
+            _userServiceAdapter = userServiceAdapter;
         }
-        private async Task<(bool exists, string userName)> UserExistsAsync(int userId)
-        {
-            var url = $"https://www.crcind.com/csp/samples/SOAP.Demo.cls?soap_method=FindPerson&id={userId}";
 
-            try
-            {
-                var response = await _httpClient.GetAsync(url);
-
-                if (response.IsSuccessStatusCode)
-                {
-                    var responseString = await response.Content.ReadAsStringAsync();
-
-
-                    if (responseString.TrimStart().StartsWith("<html>"))
-                    {
-                        Console.WriteLine("Received an HTML response, likely an error page.");
-                        return (false, null);
-                    }
-
-                    try
-                    {
-                        var xmlDoc = new XmlDocument();
-                        xmlDoc.LoadXml(responseString);
-
-
-                        var namespaceManager = new XmlNamespaceManager(xmlDoc.NameTable);
-                        namespaceManager.AddNamespace("SOAP-ENV", "http://schemas.xmlsoap.org/soap/envelope/");
-                        namespaceManager.AddNamespace("tempuri", "http://tempuri.org");
-
-
-                        var findPersonResponseNode = xmlDoc.SelectSingleNode("//SOAP-ENV:Body/tempuri:FindPersonResponse", namespaceManager);
-                        if (findPersonResponseNode != null && findPersonResponseNode.HasChildNodes)
-                        {
-                            var nameNode = findPersonResponseNode.SelectSingleNode("tempuri:FindPersonResult/tempuri:Name", namespaceManager);
-                            if (nameNode != null)
-                            {
-                                return (true, nameNode.InnerText);
-                            }
-                        }
-                    }
-                    catch (XmlException ex)
-                    {
-
-                        Console.WriteLine($"XML Parsing Error: {ex.Message}");
-                        Console.WriteLine($"Response Content: {responseString}");
-                    }
-                }
-                else
-                {
-
-                    Console.WriteLine($"Error: Service returned status code {response.StatusCode}");
-                }
-            }
-            catch (HttpRequestException ex)
-            {
-
-                Console.WriteLine($"Request Error: {ex.Message}");
-            }
-
-            return (false, null);
-        }
 
         public async Task<Result<DiscountDto, IEnumerable<string>>> CreateDiscount(DiscountDto discountDto, string agentEmail)
         {
@@ -99,7 +39,7 @@ namespace CustomerService.Services
             {
                 return Result.Failure<DiscountDto, IEnumerable<string>>(new List<string> { "The agent does not exist." });
             }
-            var (userExists, userName) = await UserExistsAsync(discountDto.UserId);
+            var (userExists, userName) = await _userServiceAdapter.UserExistsAsync(discountDto.UserId);
             if (!userExists)
             {
                 return Result.Failure<DiscountDto, IEnumerable<string>>(new List<string> { "The user does not exist." });
